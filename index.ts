89749e6c980e1e3b8ef24e965cff67cec4cc8416
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-23 17:02:29 -0300

    Migración de .js a .ts

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+//Configuration .env
+dotenv.config();
+
+//Create Express Application
+const app: Express = express();
+const port: string | number = process.env.PORT || 8000;
+
+//Define First Route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome Node')
+});
+
+//Execute App
+app.listen(port, () => {
+    console.log(`GardenShoppy Server: Running at http://localhost:${port}`
+    );
+})
