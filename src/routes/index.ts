8210d53e0402429f0eb12811ca58e705e019eb86
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-03-25 01:19:58 -0300

    Routes

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import helloRouter from './HelloRouter';
+import { LogInfo } from '@/utils/logger';
+
+//Server Instance
+const server = express();
+
+//Router Instance
+const rootRouter = express.Router();
+
+//Activate for request to http://localhost:8000/api
+rootRouter.get('/', (req: Request, res: Response) => {
+    LogInfo('GET: http://localhost:8000/api')
+    res.send('Check API Restful Service');
+});
+
+//Redirections to Routers & Controller
+server.use('/', rootRouter); //http://localhost:8000/api/
+server.use('/hello', helloRouter); //http://localhost:8000/api/hello
+//Add more routes
+
+export default server;
