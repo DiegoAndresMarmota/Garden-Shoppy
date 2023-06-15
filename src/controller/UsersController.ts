b38252c86289a23b3e780245cb0a58ee60d458a6
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-06-14 23:27:49 -0400

    Creación del user: IUser

--- src/controller/UsersController.ts
+++ src/controller/UsersController.ts
@@ -3,7 +3,8 @@ import { IUserController } from './interfaces';
 import { LogSuccess, LogError, LogWarning } from '../utils/logger';
 
 //ORM - Users Collection
-import { getAllUsers, getUserByID, deleteUserByID, updateUserByID } from '../domain/orm/User.orm';
+import { getAllUsers, getUserByID, deleteUserByID, updateUserByID, createUser } from '../domain/orm/User.orm';
+import { IUser } from '@/domain/interfaces/IUser.interface';
 
 @Route("/api/users")
 @Tags("UserController")
@@ -61,7 +62,7 @@ export class UserController implements IUserController {
 
 
     @Put("/")
-    public async updateUser(@Query()id: string, user: any): Promise<any> {
+    public async updateUser(@Query()id: string, user: IUser): Promise<any> {
         
         let response: any = '';
 
@@ -76,6 +77,29 @@ export class UserController implements IUserController {
             LogWarning('[/api/users] Updated User By ID Request');
             response = {
                 message: "Please, provide a ID to updated"
+            }
+        }
+        return response;
+    }
+
+
+    @Post("/")
+    public async createUser(user: IUser): Promise<any> {
+        let response: any = '';
+
+        if (user) {
+            LogSuccess(`[/api/users] Create New User: ${user.name} `);
+            await createUser(user).then((response) => {
+                LogSuccess(`[/api/users] Create User: ${user.name}`);
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                response = {
+                    message: `User ${user.name} created successfully`
+                }
+            });
+        } else {
+            LogWarning('[/api/users] Create needs new user');
+            response = {
+                message: 'Please, provide a new user',
             }
         }
         return response;
